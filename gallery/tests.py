4bb5c43ccf64d8e8ee3373f3a110cb1cdfedfba1
rithaven <veneruth4@gmail.com>
2019-10-14 10:31:37 +0200

    Add test

--- gallery/tests.py
+++ gallery/tests.py
@@ -1,18 +1,21 @@
 from django.test import TestCase
 from .models import Image,Location,Category
 
-class CategoryTestClass(TestCase):
+class ImageTestClass(TestCase):
 
     # Set up method
     def setUp(self):
-        self.wecode= Category(name = 'food')
+        self.wecode= Image(name = 'food'description ='akako ni keza pe')
+        self.wecode.save_image()
     # Testing  instance
     def test_instance(self):
-        self.assertTrue(isinstance(self.wecode,Category))
-
+        self.assertTrue(isinstance(self.wecode,Image))
     # Testing Save Method
     def test_save_method(self):
-        self.wecode.save_category()
-        categories = Category.objects.all()
-        self.assertTrue(len(categories) > 0)
+        self.wecode.save_Image()
+        images = Image.objects.all()
+        self.assertTrue(len(images) > 0)
+
+    def test_delete_method(self):
+        self
 
