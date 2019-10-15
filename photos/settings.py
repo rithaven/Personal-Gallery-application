f61f23980f5b2acf854cd529e5bf5bccaab9339d
rithaven <veneruth4@gmail.com>
2019-10-15 12:15:48 +0200

    Add Style

--- photos/settings.py
+++ photos/settings.py
@@ -137,7 +137,7 @@ USE_TZ =True
 #prodect.com/en/1.11/howto/static-files/
 STATIC_URL= '/static/'
 STATICFILES_DIRS=(
-    os.path.join(BASE_DIR, 'static')
+    os.path.join(BASE_DIR, 'static'),
 )
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
